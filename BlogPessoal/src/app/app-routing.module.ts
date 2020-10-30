bf8c320b9bdfb3b896611a80fae19f4fafec0b81
unknown <matheusjf330@gmail.com>
2020-10-30 18:15:15 -0300

    Criação das rotas

--- BlogPessoal/src/app/app-routing.module.ts
+++ BlogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: 'Inicio', pathMatch:'full'},
+  { path: 'Inicio' , component: HomeComponent },
+  { path: 'Interação', component: FeedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
