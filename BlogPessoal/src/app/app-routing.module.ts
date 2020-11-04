69e6992a66fa816aea255680cbdf82b4de983c23
unknown <matheusjf330@gmail.com>
2020-11-04 17:33:39 -0300

    Criacao dos services

--- BlogPessoal/src/app/app-routing.module.ts
+++ BlogPessoal/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path:'', redirectTo: 'Inicio', pathMatch:'full'},
   { path: 'Inicio' , component: HomeComponent },
-  { path: 'Interação', component: FeedComponent }
+  { path: 'Interação', component: FeedComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'cadastro', component: CadastroComponent}
 ];
 
 @NgModule({
